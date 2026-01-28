7b8cc7c83e1ffdec7d815b0362bea533719edd5f
Haizlbliek <linus.denning@gmail.com>
2026-01-28 14:38:00 -0500

    Fix EoCFanSpeed

--- Code/src/effects/EoCFanSpeed.cs
+++ Code/src/effects/EoCFanSpeed.cs
@@ -11,13 +11,11 @@ public static class EoCFanSpeed {
 	}
 
 	private static void On_SpinningFan_Update(On.SpinningFan.orig_Update orig, SpinningFan self, bool eu) {
-		float speed = self.speed;
+		orig(self, eu);
 		RoomSettings.RoomEffect effect = self.room.roomSettings.GetEffect(Enums.EoCFanSpeedEffect);
 		if (effect != null) {
 			RainCycle cycle = self.room.world.rainCycle;
-			speed = 1f + 25f * effect.amount * Mathf.Clamp01(0.75f - cycle.AmountLeft);
+			self.FanElement.rotation += self.speed * 0.1f * 25f * effect.amount * Mathf.Clamp01((0.25f - cycle.AmountLeft) / 0.25f);
 		}
-		orig(self, eu);
-		self.speed = speed;
 	}
 }
